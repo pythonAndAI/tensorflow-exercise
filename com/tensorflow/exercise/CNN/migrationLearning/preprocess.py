277a2f7d34c7f701d023a583e84d760412ca1201
hawazi <951965096@qq.com>
2019-03-14 23:16:42 +0800

    migration learning

--- com/tensorflow/exercise/CNN/migrationLearning/preprocess.py
+++ com/tensorflow/exercise/CNN/migrationLearning/preprocess.py
@@ -2,7 +2,7 @@ import tensorflow as tf
 import numpy as np
 import os.path
 import glob
-from com.tensorflow.exercise.logging import LOG
+# from com.tensorflow.exercise.logging import LOG
 
 #原始输入数据的目录，这个目录底下有5个子目录，每个子目录底下保存属于该类别的所有图片
 INPUT_PATH = "E:\\Alls\\software\\flower_photo\\flower_photos"
@@ -17,7 +17,7 @@ TEST_PERCENTAGE = 10
 
 def create_image_lists(sess=None, testing_percentage=None, validation_percentage=None):
     #获取一个元组，第一位是文件夹路径，第二位是一个list集合，里面是文件夹底下所有图片的名称
-    sub_dirs = [x[0] for x in os.walk(INPUT_PATH)]
+    # sub_dirs = [x[0] for x in os.walk(INPUT_PATH)]
     # LOG.getlogger("sub_dirs").info(sub_dirs)
 
     #初始化各个数据集
@@ -29,13 +29,16 @@ def create_image_lists(sess=None, testing_percentage=None, validation_percentage
     validation_labels = []
     current_label = 0
     #遍历获取的文件路径列表
+    sub_dirs = ["E:\\Alls\\software\\flower_photo\\flower_photos\\test"]
     for sub_dir in sub_dirs:
         #获取一个子目录中所有的图片文件
         extensions = ['jpg', 'jpeg', 'JPG', 'JPEG']
         file_list = []
+        # LOG.getlogger("sub_dir").info(sub_dir)
         #得到文件夹名称
         dir_name = os.path.basename(sub_dir)
         for extension in extensions:
+            # LOG.getlogger("extension").info(extension)
             #拼接路径。比如：E:\Alls\software\flower_photo\flower_photos\daisy\.jpg
             file_glob = os.path.join(INPUT_PATH, dir_name, '*.' + extension)
             #获取file_glob路径底下所有的图片的文件名。并赋给file_list
@@ -45,6 +48,7 @@ def create_image_lists(sess=None, testing_percentage=None, validation_percentage
 
             #处理图片数据
             for file_name in file_list:
+                # LOG.getlogger("current_label").info(current_label)
                 #读取并解析图片，将图片转化为299*299以便inception-v3模型来处理
                 image_raw_data = tf.gfile.FastGFile(file_name, 'rb').read()
                 image = tf.image.decode_jpeg(image_raw_data)
@@ -79,6 +83,7 @@ def main():
     with tf.Session() as sess:
         processed_data = create_image_lists(sess, VALIDATION_PERCENTAGE, TEST_PERCENTAGE)
         #通过numpy格式保存后处理的数据
+        # LOG.getlogger("output_data").info(processed_data)
         np.save(OUTPUT_FILE, processed_data)
 
 if __name__ == "__main__":
